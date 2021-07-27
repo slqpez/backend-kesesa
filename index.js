1be010b860a8fd47923c00b08235bf626389bdb8
slqpez <santiago.lopez16@udea.edu.co>
2021-07-27 17:09:09 -0500

    Simple qoute

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ app.use(passport.session());
 
 app.use(
   cors({ 
-    origin: ["https://frontend-kesesa.vercel.app", "http://localhost:3000"], //TODO cambiar cuando se despliegue el front.
-    credentials: true,
-    method: "GET, POST, PUT, DELETE, PATCH"
+     //TODO cambiar cuando se despliegue el front.
+    origin: [ 'https://frontend-kesesa.vercel.app', 'http://localhost:3000'],
+    credentials: true
   })
 ); 
 
