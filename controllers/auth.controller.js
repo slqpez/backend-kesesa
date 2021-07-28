c995bf9b575f97df631d4dbafe87ea1e0acd2d52
slqpez <santiago.lopez16@udea.edu.co>
2021-07-28 14:55:32 -0500

    Deleted same origin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,32 +1,28 @@
+const authController = {
+  loginSucces: (req, res) => {
+    if (req.user) {
+      return res.status(200).json({
+        success: true,
+        message: "user has successfully authenticated",
+        user: req.user,
+        cookies: req.cookies,
+      });
+    } else {
+      return res.status(400).json({ message: "Dont logged." });
+    }
+  },
 
-const authController ={
+  loginFailed: (req, res) => {
+    return res.status(401).json({
+      success: false,
+      message: "user failed to authenticate.",
+    });
+  },
 
-    loginSucces:  (req, res) => {
-        if (req.user) {
-         return res.status(200).json({
-            success: true,
-            message: "user has successfully authenticated",
-            user: req.user,
-            cookies: req.cookies,
-          })
-        }else{
-          return res.status(400).json({message: "Dont logged."})
-        }
-      },
-
-      loginFailed: (req, res) => {
-       return res.status(401).json({
-          success: false,
-          message: "user failed to authenticate.",
-        });
-      },
-
-      logout: (req, res) => {
-        req.logout();
-       return res.redirect("http://localhost:3000")
-      },
-
-      
-}
+  logout: (req, res) => {
+    req.logout();
+    return res.redirect("http://localhost:3000");
+  },
+};
 
 module.exports = authController;
--- index.js
+++ index.js
@@ -20,9 +20,7 @@ app.use(
   cookieSession({
     name: "session",
     keys: [process.env.SECRET_KEY],
-    maxAge: 24 * 60 * 60 * 100,
-    secure: true,
-    sameSite:"none"
+    maxAge: 24 * 60 * 60 * 100
   })
 );
 
